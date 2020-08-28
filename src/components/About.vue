20a188e7c697e976bacf055ec4a6b971a72de9a9
xudunyao <xudunyao@126.com>
2020-08-28 22:24:43 +0800

    os 提交

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,5 +3,6 @@
          <iframe class="filename" src="https://view.officeapps.live.com/op/view.aspx?src=https://misuyunpu.oss-cn-shenzhen.aliyuncs.com/New%20XLSX%20%E5%B7%A5%E4%BD%9C%E8%A1%A8.xlsx" width="100%" height="600" ></iframe>
          <div>我是新增加的内容</div>
          <div>WINDOW我是新增加的内容</div>
+          <div>OS我是新增加的内容</div>
     </div>
 </template>
