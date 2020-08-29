06a8a8b1680c6314b76cde4c5fbffebcbff4f21e
xuyao <361803548@qq.com>
2020-08-29 23:42:34 +0800

    WINDOW1

--- src/components/Student.vue
+++ src/components/Student.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="student">
         <iframe class="filename" src="https://misuyunpu.oss-cn-shenzhen.aliyuncs.com/%E8%AE%B8%E6%95%A6%E9%81%A5-%E5%89%8D%E7%AB%AF%E5%BC%80%E5%8F%91.pdf" width="100%" height="600" ></iframe>
-        
+        <span>wondow1</span>
     </div>
 </template>
