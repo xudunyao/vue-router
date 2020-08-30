92871115af6e61bcf7ab046058e1905bb682fcd0
xuyao <361803548@qq.com>
2020-08-30 12:44:31 +0800

    第二个圆点

--- src/components/Learn.vue
+++ src/components/Learn.vue
@@ -2,5 +2,6 @@
     <div class="learn">
         <iframe class="filename" src="https://misuyunpu.oss-cn-shenzhen.aliyuncs.com/09595250.jpg" width="100%" height="600"></iframe>
         <div>111111</div>
+        <div>222222</div>
     </div>
 </template>
